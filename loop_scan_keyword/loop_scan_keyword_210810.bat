a9b5eaeff040172c7284ec8f97c32b7e7eb28716
Wayne.Chiu <Wayne.Chiu@advantech.com.tw>
2021-08-10 16:30:35 +0800

    update comment to explain how it work. refer: https://ss64.com/nt/for_f.html

--- loop_scan_keyword/loop_scan_keyword_210810.bat
+++ loop_scan_keyword/loop_scan_keyword_210810.bat
@@ -5,26 +5,49 @@ ECHO ON
 
 @REM  "refer: https://blog.miniasp.com/post/2010/09/24/How-to-parse-text-from-file-or-command-using-Batch"
 @REM  "refer: https://ss64.com/nt/for_f.html"
+@REM  "refer: https://www.robvanderwoude.com/for.php"
+@REM  "refer: https://www.robvanderwoude.com/if.php"
+@REM  "refer: https://ss64.com/nt/for_f.html"
+@REM  =============================================================
+@REM  Purpose:
 @REM  PRJ_NAME is a 8 bytes string. 
-@REM  We parsing it for 8 loop to check the parameters.
-@REM  the TOEKNS: 
-@REM  1 : %%I
-@REM  2 : %%J
-@REM  3 : %%K
-@REM  4 : %%L
-@REM  5 : %%M
-@REM  6 : %%N
-@REM  7 : %%O
-@REM  8 : %%P
-@REM  9 : %%Q
-@REM  10: %%R
-@REM  11: %%S
-@REM  12: %%T
-@REM  In order to parser the input string or file use the parameter : /F
-@REM  the conditin use "......" to set parser condition.
-@REM  EOL=/n or others, End of line condition.
-@REM  TOEKN is same as upper descripton number, exception * character, it means the remain data in one line. (pass throught the data)
-@REM  DELTMS: the delimiter to cut off the string point. 
+@REM  We parsing it to check the parameters.
+@REM  =============================================================
+@REM  Syntax 
+@REM          FOR /F ["options"] %%parameter IN (filenameset) DO command
+@REM          FOR /F ["options"] %%parameter IN ("Text string to process") DO command
+@REM  =============================================================
+@REM  Key
+@REM     options:
+@REM         delims=X  the delimiter character(s). Default for string = a space or TAB.
+@REM         skip=n    A number of lines to skip at the beginning of file. (default = 0)
+@REM         eol=;     character at the start of each line to indicate a comment
+@REM                   the defult is a semicolon(;)
+@REM         tokens=n  specifices which numbered items to read from each line (default = 1)
+@REM                
+@REM                   1 : %%I
+@REM                   2 : %%J
+@REM                   3 : %%K
+@REM                   4 : %%L
+@REM                   5 : %%M
+@REM                   6 : %%N
+@REM                   7 : %%O
+@REM                   8 : %%P
+@REM                   9 : %%Q
+@REM                   10: %%R
+@REM                   11: %%S
+@REM                   12: %%T
+@REM         usebackq  use the alternate quoting style:
+@REM                   - Instead of double quotes use single quotes for 'text string to process'
+@REM                   - Use double quotes for long file names in "filenameset".
+@REM                   - Use back quotes for `command to process`
+@REM  Filenameset      A set of one or more files, enclosed in parentheses (file1,file2).
+@REM  command          The command to carry out, including any parameters.
+@REM                   This can be single command, or if you enclose it 
+@REM                   in  (parentheses), several commands, one per line.
+@REM  %%parameter      A replaceable parameter:
+@REM                   in a batch file use %%G (on the command line %G)
+@REM                   (staring at I) (in here, it needs add double-'%' for use ex. %%I)
 
 @REM Find the line const char PRJ_NAME[8]={'X','C','5',' ','+','-','_','6'};
 FOR /F "EOL=/ TOKENS=1,2,3,4,5,6,7,8,9,10,11,12,13* DELIMS=',[}; " = %%I IN (sample.txt) DO (
